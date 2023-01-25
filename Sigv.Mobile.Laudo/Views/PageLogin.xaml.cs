c9c4926c97d8da45c3285014c0ad2aa5d1ae5ceb
Diego Maia <diego.maia83@gmail.com>
2023-01-25 18:12:53 -0300

    Laudo - Tela de Login / Home"
    This reverts commit d4439fea6cffdefaacfa17a984831c8a5f454818.

--- Sigv.Mobile.Laudo/Views/PageLogin.xaml.cs
+++ Sigv.Mobile.Laudo/Views/PageLogin.xaml.cs
@@ -1,7 +1,3 @@
-using Sigv.Domain;
-using Sigv.Mobile.Laudo.Aplicacao;
-using Sigv.Mobile.Laudo.Services;
-
 namespace Sigv.Mobile.Laudo.Views;
 
 public partial class PageLogin : ContentPage
@@ -10,48 +6,4 @@ public partial class PageLogin : ContentPage
 	{
 		InitializeComponent();
 	}
-
-    private void LoginButton_Clicked(object sender, EventArgs e)
-    {
-        var userName = UsernameEntry.Text;
-        var password = PasswordEntry.Text;
-
-
-        try
-        {
-            using (var client = new HttpClient())
-            {
-                var senhaEncr = Aplicacao.Util.AESEncrytDecry.EncryptStringAES(password);
-
-                var loginEfetuado = (MensagemRetorno)new AuthService().RetornarLoginToken(userName, senhaEncr);
-
-                if (loginEfetuado.Sucesso)
-                {
-                    using (var srv = new HttpService<Acesso>())
-                    {
-                        var acesso = new Acesso()
-                        {
-                            UsuarioId = Convert.ToInt32(UserPreferences.Logado.UsuarioId),
-                            Ip = UserPreferences.Logado.Ip,
-                            Data = DateTime.Now
-                        };
-
-                        srv.ExecuteService(acesso, "api/acesso/salvar");
-
-
-                        Application.Current.MainPage = new PageHome();
-                    }
-
-                }
-                else
-                {
-                    DisplayAlert("Resultado da operação", loginEfetuado.Mensagem, "OK");
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            DisplayAlert("Resultado da operação", "Houve um erro ao processar a rotina", "OK");
-        }
-    }
 }
